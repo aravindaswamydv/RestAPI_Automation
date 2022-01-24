7af6e2eb7c88c23d9898f53e56e4c97f8ccfd6cd
aravindaswamydv <aravindaswamydv@gmail.com>
2022-01-24 21:40:48 +0530

    API Automation using Java Http client - Initial Commit

---
+++ src/test/java/com/qa/tests/GetAPITest.java
@@ -0,0 +1,137 @@
+package com.qa.tests;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.http.Header;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.client.RestCleint;
+import com.qa.utils.TestUtil;
+
+import qa.com.base.TestBase;
+
+public class GetAPITest extends TestBase {
+TestBase testBase;
+RestCleint restClient;
+String serviceURL,apiURL,url;
+CloseableHttpResponse closeableHttpResponse;
+
+@BeforeMethod
+public void setUp() {
+	testBase = new TestBase();
+	serviceURL = prop.getProperty("URL");
+	apiURL=prop.getProperty("serviceURL");
+	
+	url = serviceURL + apiURL;
+	
+}
+
+@Test(priority=1)
+public void getAPITestWithoutHeaders() throws ClientProtocolException, IOException {
+	restClient = new RestCleint();
+	closeableHttpResponse=restClient.get(url);
+	
+	// a. Status Code:
+	int statusCode= closeableHttpResponse.getStatusLine().getStatusCode();
+	System.out.println("http status code --> " + statusCode);
+	
+	
+	Assert.assertEquals(statusCode,RESPONSE_STATUS_CODE_200,"Status Code is not 200");
+	
+	String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+	
+	// b. Json String: 
+	JSONObject responseJson= new JSONObject(responseString);
+	System.out.println("Response JSON from API -- " + responseJson);
+	
+	// Single value Assertion
+	String perPageValue=TestUtil.getValuesByJPath(responseJson, "/per_page");
+	System.out.println("Value of PerPage -- " + perPageValue);
+	Assert.assertEquals(Integer.parseInt(perPageValue), 6);
+	
+	String tot=TestUtil.getValuesByJPath(responseJson, "/total");
+	System.out.println("Value of PerPage -- " + tot);
+	Assert.assertEquals(Integer.parseInt(tot), 12);
+	
+	// get the value from JSON ARRAY
+	
+	String lastName=TestUtil.getValuesByJPath(responseJson, "/data[0]/last_name");
+	String id=TestUtil.getValuesByJPath(responseJson, "/data[0]/id");
+	String avatar=TestUtil.getValuesByJPath(responseJson, "/data[0]/avatar");
+	String firstName=TestUtil.getValuesByJPath(responseJson, "/data[0]/first_name");
+	
+	System.out.println(lastName);
+	System.out.println(id);
+	System.out.println(avatar);
+	System.out.println(firstName);
+	
+
+	// c. All Headers:
+	Header[] headerArry = closeableHttpResponse.getAllHeaders();
+	HashMap<String,String> allHeaders = new HashMap<String,String>();
+	
+	for(Header header:headerArry) {
+		allHeaders.put(header.getName(), header.getValue());
+	}
+	System.out.print("Headers Array -- " + allHeaders);
+}
+
+@Test(priority=2)
+public void getAPITestWithHeaders() throws ClientProtocolException, IOException {
+	restClient = new RestCleint();
+	HashMap<String,String> headerMap=new HashMap<String,String>();
+	headerMap.put("Content-Type","application/json");
+	/*headerMap.put("username","testuser");
+	headerMap.put("password","test1234");
+	headerMap.put("Auth Token","12345");*/
+	
+	closeableHttpResponse=restClient.get(url,headerMap);
+	
+	// a. Status Code:
+	int statusCode= closeableHttpResponse.getStatusLine().getStatusCode();
+	System.out.println("http status code --> " + statusCode);
+	
+	
+	Assert.assertEquals(statusCode,RESPONSE_STATUS_CODE_200,"Status Code is not 200");
+	
+	String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+	
+	// b. Json String: 
+	JSONObject responseJson= new JSONObject(responseString);
+	System.out.println("Response JSON from API -- " + responseJson);
+	
+	// Single value Assertion
+	String perPageValue=TestUtil.getValuesByJPath(responseJson, "/per_page");
+	System.out.println("Value of PerPage -- " + perPageValue);
+	Assert.assertEquals(Integer.parseInt(perPageValue), 6);
+	
+	// get the value from JSON ARRAY
+	
+	String lastName=TestUtil.getValuesByJPath(responseJson, "/data[0]/last_name");
+	String id=TestUtil.getValuesByJPath(responseJson, "/data[0]/id");
+	String avatar=TestUtil.getValuesByJPath(responseJson, "/data[0]/avatar");
+	String firstName=TestUtil.getValuesByJPath(responseJson, "/data[0]/first_name");
+	
+	System.out.println(lastName);
+	System.out.println(id);
+	System.out.println(avatar);
+	System.out.println(firstName);
+	
+	// c. All Headers:
+	Header[] headerArry = closeableHttpResponse.getAllHeaders();
+	HashMap<String,String> allHeaders = new HashMap<String,String>();
+	
+	for(Header header:headerArry) {
+		allHeaders.put(header.getName(), header.getValue());
+	}
+	System.out.print("Headers Array -- " + allHeaders);
+}
+
+}
---
+++ src/test/java/com/qa/tests/PostAPITest.java
@@ -0,0 +1,85 @@
+package com.qa.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.client.RestCleint;
+import com.qa.data.Users;
+
+import qa.com.base.TestBase;
+
+public class PostAPITest extends TestBase{
+	
+	TestBase testBase;
+	String serviceURL,apiURL,url;
+	CloseableHttpResponse closeableHttpResponse;
+	RestCleint restClient;
+	
+	@BeforeMethod
+	public void setUP() {
+	testBase = new TestBase();	
+	serviceURL = prop.getProperty("URL");
+	apiURL=prop.getProperty("serviceURL");
+	
+	url = serviceURL + apiURL;
+	}
+	
+	@Test
+	public void postAPITest() throws StreamWriteException, DatabindException, IOException {
+		restClient=new RestCleint();
+		HashMap<String,String> headerMap=new HashMap<String,String>();
+		headerMap.put("Content-Type","application/json");
+		
+		// jackson API
+		// Marshaling and un-marshaelling - convert java object to json object and vice versa
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Users users = new Users("morpheus","leader");  // expected users Object
+		
+		// object to json file
+		mapper.writeValue(new File("C:\\eclipseWorkspace3\\Automation_RestApi\\src\\main\\java\\com\\qa\\data\\user.json"),users);
+		
+		// java Object to Json in String
+		String userJsonString = mapper.writeValueAsString(users);
+		System.out.println(userJsonString);
+		
+		closeableHttpResponse=restClient.post(url, userJsonString, headerMap);	
+		
+
+		// a. Status Code:
+		int statusCode= closeableHttpResponse.getStatusLine().getStatusCode();
+		System.out.println("http status code --> " + statusCode);
+		
+		
+		Assert.assertEquals(statusCode,RESPONSE_STATUS_CODE_201,"Status Code is not 201");
+		
+		// JSON String
+		String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+		
+		JSONObject responseJson=new JSONObject(responseString);
+		System.out.println("Response from API is --> " + responseJson);
+		
+		// Json to Java object
+		Users userResObject = mapper.readValue(responseString, Users.class); // actual user Object
+		System.out.println(userResObject);
+		
+		Assert.assertTrue(users.getName().equals(userResObject.getName()));
+		
+		Assert.assertTrue((users.getJob()).equals(userResObject.getJob()));
+		System.out.println(userResObject.getId());
+		System.out.println(userResObject.getCreatedAt());
+	}
+
+}
